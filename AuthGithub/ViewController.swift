7ba5509be77c191f2d0a54a45f39e867e0791cf4
Loriens <lerd.95@mail.ru>
2019-03-03 21:32:02 +0300

    Initial Commit

---
+++ AuthGithub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AuthGithub
+//
+//  Created by Vladislav on 03/03/2019.
+//  Copyright © 2019 Vladislav Markov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
