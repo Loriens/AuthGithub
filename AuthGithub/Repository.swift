e3a6226cf6fc6c8a8c040b02771d9726308ffb41
Loriens <lerd.95@mail.ru>
2019-03-05 17:40:05 +0300

    Fix struct Repository

--- AuthGithub/Repository.swift
+++ AuthGithub/Repository.swift
@@ -11,13 +11,14 @@ import Foundation
 struct Repository {
     private(set) var title: String
     private(set) var description: String
+    private(set) var author: String
     private(set) var owner: Owner
     
     struct Owner {
         private(set) var author: String
         
-        init? (json: [Dictionary<String, Any>]) {
-            guard let login = json[0]["login"] as? String else {
+        init? (json: Dictionary<String, Any>) {
+            guard let login = json["login"] as? String else {
                 return nil
             }
             
@@ -28,13 +29,14 @@ struct Repository {
     init? (json: [String: Any]) {
         guard let title = json["name"] as? String,
             let description = json["description"] as? String,
-            let owner = json["owner"] as? [Dictionary<String, Any>] else {
+            let owner = json["owner"] as? Dictionary<String, Any> else {
             return nil
         }
         
         self.title = title
         self.description = description
         self.owner = Owner(json: owner)!
+        self.author = self.owner.author
     }
     
     
