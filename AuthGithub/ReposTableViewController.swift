821c948268059e5b39ae08e6cb074b86fe9aa1e3
Loriens <lerd.95@mail.ru>
2019-03-05 15:22:01 +0300

    Added ReposTableViewController

---
+++ AuthGithub/ReposTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReposTableViewController.swift
+//  AuthGithub
+//
+//  Created by Vladislav on 05/03/2019.
+//  Copyright © 2019 Vladislav Markov. All rights reserved.
+//
+
+import UIKit
+
+class ReposTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
