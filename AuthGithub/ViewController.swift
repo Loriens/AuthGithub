5a9ce47cfc51b7ac5b2164529572747eed48978e
Loriens <lerd.95@mail.ru>
2019-03-07 15:20:01 +0300

    Add saving login and password to Keychain. If Keychain consists login and password, the app log in.

--- AuthGithub/ViewController.swift
+++ AuthGithub/ViewController.swift
@@ -20,6 +20,13 @@ class LoginViewController: UIViewController {
         
         let logoURL = URL(string: "https://i2.wp.com/www.globalemancipation.ngo/wp-content/uploads/2017/09/github-logo.png")
         logoView.kf.setImage(with: logoURL)
+        
+        if let jsonData = self.readAnyAccount() {
+            let decoder = JSONDecoder()
+            let account = try? decoder.decode(Account.self, from: jsonData)
+            
+            self.loginPressed(account)
+        }
     }
 
     @IBAction func loginPressed(_ sender: Any) {
@@ -28,9 +35,20 @@ class LoginViewController: UIViewController {
             return
         }
         
-        guard let username = usernameField.text,
-            let password = passwordField.text else {
-            print("Enter username and password")
+        var usernameTmp: String?,
+        passwordTmp: String?
+        
+        if let account = sender as? Account {
+            usernameTmp = account.username
+            passwordTmp = account.password
+        } else if usernameField.text != nil && passwordField.text != nil {
+            usernameTmp = usernameField.text
+            passwordTmp = passwordField.text
+        }
+        
+        guard let username = usernameTmp,
+            let password = passwordTmp else {
+            print("Enter login and password")
             return
         }
         
@@ -69,6 +87,13 @@ class LoginViewController: UIViewController {
                     return
                 }
                 
+                // Сохраняем логин и пароль в Keychain
+                if self.readAnyAccount() == nil {
+                    if self.saveAccount(account: Account(username: username, password: password)) {
+                        print("Login and password have added to Keychain")
+                    }
+                }
+                
                 // Переходим на вью поиска при успешной авторизации
                 DispatchQueue.main.async {
                     let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -88,8 +113,64 @@ class LoginViewController: UIViewController {
     
 }
 
-extension String {
+// Service name for Keychain
+private let service = "AuthGithub"
+
+// Keychain functions
+extension LoginViewController {
     
+    private func keychainQuery() -> [String : AnyObject] {
+        var query = [String : AnyObject]()
+        query[kSecClass as String] = kSecClassGenericPassword
+        query[kSecAttrAccessible as String] = kSecAttrAccessibleWhenUnlocked
+        query[kSecAttrService as String] = service as AnyObject
+        
+        return query
+    }
+    
+    private func readAnyAccount() -> Data? {
+        var query = keychainQuery()
+        query[kSecMatchLimit as String] = kSecMatchLimitOne
+        query[kSecReturnData as String] = kCFBooleanTrue
+        query[kSecReturnAttributes as String] = kCFBooleanTrue
+        
+        var queryResult: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, UnsafeMutablePointer(&queryResult))
+        
+        if status != noErr {
+            return nil
+        }
+        
+        guard let item = queryResult as? [String : AnyObject],
+            let accountData = item[kSecValueData as String] as? Data else {
+                return nil
+        }
+        
+        return accountData
+    }
+    
+    private func saveAccount(account: Account) -> Bool {
+        let encoder = JSONEncoder()
+        let accountData = try? encoder.encode(account)
+        
+        if readAnyAccount() != nil {
+            var attributesToUpdate = [String : AnyObject]()
+            attributesToUpdate[kSecValueData as String] = accountData as AnyObject
+            
+            let query = keychainQuery()
+            let status = SecItemUpdate(query as CFDictionary, attributesToUpdate as CFDictionary)
+            return status == noErr
+        }
+        
+        var item = keychainQuery()
+        item[kSecValueData as String] = accountData as AnyObject
+        let status = SecItemAdd(item as CFDictionary, nil)
+        return status == noErr
+    }
+    
+}
+
+extension String {
     // Функция кодирования String в base64
     func base64Encoded() -> String? {
         if let data = self.data(using: String.Encoding.utf8) {
