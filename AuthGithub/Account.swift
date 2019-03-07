f6f337aaeb4e7deee9d7728f35fd3edcd681f680
Loriens <lerd.95@mail.ru>
2019-03-07 14:44:43 +0300

    Add struct Account

---
+++ AuthGithub/Account.swift
@@ -0,0 +1,14 @@
+//
+//  Account.swift
+//  AuthGithub
+//
+//  Created by Vladislav on 07/03/2019.
+//  Copyright © 2019 Vladislav Markov. All rights reserved.
+//
+
+import Foundation
+
+struct Account: Codable {
+    var username: String
+    var password: String
+}
